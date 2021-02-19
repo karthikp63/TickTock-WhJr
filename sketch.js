ffc3250c5caf6e3cd510b387f85cfe3b015e3b72
karthikp63 <73623494+karthikp63@users.noreply.github.com>
2021-02-19 15:41:37 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+  createCanvas(450,450);
+  angleMode(DEGREES);
+}
+
+function draw(){ 
+background(0);
+translate(200,200)
+rotate (-90);
+
+  hr = hour();
+  mn = minute();
+  sc = second();
+  
+  strokeWeight(8);
+  stroke("yellow");
+  noFill();
+  end = map(sc,0,60,0,360);
+  arc(0,0,300,300,0,end);
+  
+  stroke("orange");
+  end2 = map(mn,0,60,0,360);
+  arc(0,0,280,280,0,end2);
+  stroke("light blue");
+  end3 = map(hr%12,0,12,-0,360);
+  arc(0,0,260,260,0,end3);
+  push();
+  rotate(end);
+  stroke("blue");
+  line(0,0,100,0);
+  pop();
+  push();
+  rotate(end2);
+  stroke("red");
+  line(0,0,75,0);
+  pop();
+  push();
+  rotate(end3);
+  stroke("green");
+  line(0,0,50,0);
+  pop();
+  stroke("Black");
+  point (0,0);
+  }
